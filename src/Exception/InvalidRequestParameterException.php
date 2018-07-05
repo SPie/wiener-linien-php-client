6d9d3a412bf42db87c6fe2203e08c030f664fb53
SPie <d00pl3x@gmail.com>
2018-07-05 23:01:00 +0200

    added missing exception

---
+++ src/Exception/InvalidRequestParameterException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SPie\WienerLinien\Exception;
+
+/**
+ * Class InvalidRequestParameterException
+ *
+ * @package SPie\WienerLinien\Exception
+ */
+class InvalidRequestParameterException extends \Exception
+{
+
+}
